71d91b2a33a611fa8fe91d49bf7df1fea1575f5b
Lazar Spasojevic <spasojevic2110@gmail.com>
2022-09-04 15:03:54 +0200

    Initial commit, project 'healthcare-management' created !

---
+++ healthcare-be/src/test/java/it/lazar/spasojevic/healthcarebe/HealthcareBeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.lazar.spasojevic.healthcarebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
